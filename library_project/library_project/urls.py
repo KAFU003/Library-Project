d50104554bb4718ef0e99a792fb9bd59249bb36c
KAFU003 <11056034@ntub.edu.tw>
2023-10-28 19:01:52 +0800

    change to old

--- library_project/library_project/urls.py
+++ library_project/library_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library_project project.
+URL configuration for mblog0927 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite import views as mv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mv.homepage, name="homepage"),
+    path('post/<slug:slug>/', mv.showpost, name="showpost"),
 ]
