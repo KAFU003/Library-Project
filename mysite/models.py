9e36c7bfeab12a5d0c0f4ea6cee8c324e6da0924
KAFU003 <11056034@ntub.edu.tw>
2023-10-29 17:36:25 +0800

    v1.2.0

--- mysite/models.py
+++ mysite/models.py
@@ -6,8 +6,9 @@ class Post(models.Model):
     author = models.CharField(max_length = 200, default = 'who?')
     slug = models.CharField(max_length = 200)
     chapter = models.CharField(max_length = 200, default='第?章')
-    body = models.TextField(max_length = 1000)
+    body = models.TextField(max_length = 10000)
     pub_date = models.CharField(max_length = 200)
+    image_url = models.URLField(blank=True, null=True)
     
     class Meta:
         ordering = ('-pub_date', )
