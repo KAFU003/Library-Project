171d4067667ca4a88bee2dcf67752413ff5ae791
KAFU003 <11056034@ntub.edu.tw>
2023-10-29 14:18:33 +0800

    v1.0.1

--- mysite/models.py
+++ mysite/models.py
@@ -5,8 +5,8 @@ class Post(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 200, default = 'who?')
     slug = models.CharField(max_length = 200)
-    chapter = models.CharField(max_length = 200, default='第一章')
-    body = models.TextField(max_length = 200)
+    chapter = models.CharField(max_length = 200, default='第?章')
+    body = models.TextField(max_length = 1000)
     pub_date = models.CharField(max_length = 200)
     
     class Meta:
