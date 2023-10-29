53a936f7c36b59318fc2f4e485791134b037e6b3
KAFU003 <11056034@ntub.edu.tw>
2023-10-29 11:43:11 +0800

    edit 1

--- mysite/models.py
+++ mysite/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200, default = 'who?')
     slug = models.CharField(max_length = 200)
-    body = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    chapter = models.CharField(max_length = 200, default='第一章')
+    body = models.TextField(max_length = 200)
+    pub_date = models.CharField(max_length = 200)
     
     class Meta:
         ordering = ('-pub_date', )
